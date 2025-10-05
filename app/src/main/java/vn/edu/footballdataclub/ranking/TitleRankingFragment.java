eca38b2805084f3f6cb5546288f2857cafaabaa0
ThangNS32 <hanhlinhdisney2005@gmail.com>
2025-10-06 01:07:29 +0700

    Europe Ranking (Thang)

---
+++ app/src/main/java/vn/edu/footballdataclub/ranking/TitleRankingFragment.java
@@ -0,0 +1,32 @@
+package vn.edu.footballdataclub.ranking;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.edu.footballdataclub.R;
+
+public class TitleRankingFragment extends Fragment {
+
+    public TitleRankingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title_ranking, container, false);
+    }
+}
